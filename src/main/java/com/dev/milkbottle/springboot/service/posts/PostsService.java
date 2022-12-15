8351eee66e299e04bb4a606cbfe6116ec6b7ded4
milkbottle0305 <bigtog0602@gmail.com>
2022-12-15 22:54:07 +0900

    Feat: PostsController에 update, findById 기능 추가
    update를 위해 PoststUpdateRequestDto, PostsResponseDto를 추가
    update, findById 기능 추가로 인한 Domain, Controller, Service 수정

--- src/main/java/com/dev/milkbottle/springboot/service/posts/PostsService.java
+++ src/main/java/com/dev/milkbottle/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.dev.milkbottle.springboot.service.posts;
 
+import com.dev.milkbottle.springboot.domain.posts.Posts;
 import com.dev.milkbottle.springboot.domain.posts.PostsRepository;
+import com.dev.milkbottle.springboot.web.dto.PostsResponseDto;
 import com.dev.milkbottle.springboot.web.dto.PostsSaveRequestDto;
+import com.dev.milkbottle.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Service;
@@ -14,7 +17,22 @@ public class PostsService {
 
     private final PostsRepository postsRepository;
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto){
+    public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(posts);
     }
 }
