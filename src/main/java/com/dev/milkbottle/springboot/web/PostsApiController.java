be5ada983c8ed43891602d5e1ef0389914e31df9
milkbottle0305 <bigtog0602@gmail.com>
2022-12-15 22:36:21 +0900

    Style: PostsApiController 오타수정
    PutMapping에서 PostMapping으로 변경

--- src/main/java/com/dev/milkbottle/springboot/web/PostsApiController.java
+++ src/main/java/com/dev/milkbottle/springboot/web/PostsApiController.java
@@ -3,6 +3,7 @@ package com.dev.milkbottle.springboot.web;
 import com.dev.milkbottle.springboot.service.posts.PostsService;
 import com.dev.milkbottle.springboot.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
-    @PutMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
