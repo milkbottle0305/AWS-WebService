0b2e8d6de1fc7ab3d0b204e963b67d3d542ea7a5
milkbottle0305 <bigtog0602@gmail.com>
2022-12-18 23:24:56 +0900

    Feat: 게시글 저장 및 목록 조회 기능 추가
    PostsListResponseDto로 findAllDesc()를 통해 내림차순 정렬
    기본 path인 "/"경로에서 글의 목록을 볼 수 있도록 Controller, Service 수정

--- src/main/java/com/dev/milkbottle/springboot/service/posts/PostsService.java
+++ src/main/java/com/dev/milkbottle/springboot/service/posts/PostsService.java
@@ -2,14 +2,16 @@ package com.dev.milkbottle.springboot.service.posts;
 
 import com.dev.milkbottle.springboot.domain.posts.Posts;
 import com.dev.milkbottle.springboot.domain.posts.PostsRepository;
+import com.dev.milkbottle.springboot.web.dto.PostsListResponseDto;
 import com.dev.milkbottle.springboot.web.dto.PostsResponseDto;
 import com.dev.milkbottle.springboot.web.dto.PostsSaveRequestDto;
 import com.dev.milkbottle.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -34,5 +36,12 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
