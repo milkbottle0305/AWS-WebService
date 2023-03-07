ddab156710de6debcf05b7cd37269ae7f5263fd8
milkbottle0305 <bigtog0602@gmail.com>
2023-03-07 15:49:00 +0900

    Feat: OAuth2 방식 로그인 기능 구현

--- src/main/java/com/dev/milkbottle/springboot/web/IndexController.java
+++ src/main/java/com/dev/milkbottle/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.dev.milkbottle.springboot.web;
 
+import com.dev.milkbottle.springboot.config.auth.dto.SessionUser;
 import com.dev.milkbottle.springboot.domain.posts.PostsRepository;
 import com.dev.milkbottle.springboot.service.posts.PostsService;
 import com.dev.milkbottle.springboot.web.dto.PostsResponseDto;
@@ -9,14 +10,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
