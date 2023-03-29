364b1dc0f7f59bc27e1990e400ee5323336bd304
milkbottle0305 <bigtog0602@gmail.com>
2023-03-29 13:10:10 +0900

    Update ProfileController.java

--- src/main/java/com/dev/milkbottle/springboot/web/ProfileController.java
+++ src/main/java/com/dev/milkbottle/springboot/web/ProfileController.java
@@ -14,11 +14,15 @@ public class ProfileController {
     private final Environment env;
 
     @GetMapping("/profile")
-    public String profile(){
+    public String profile() {
         List<String> profiles = Arrays.asList(env.getActiveProfiles());
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
-        String defaultProfile = profiles.isEmpty()? "default":profiles.get(0);
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
+        return profiles.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
     }
+}
 }
