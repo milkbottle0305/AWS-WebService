7c04a2b3f266cdad9d3ca68be7959192fdd91c8d
milkbottle0305 <bigtog0602@gmail.com>
2023-03-30 08:36:53 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,7 +5,7 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
-PROJECT_NAME=freelec-springboot2-webservice
+PROJECT_NAME=AWS-WebService
 
 echo "> Build 파일 복사"
 echo "> cp $REPOSITORY/zip/*.jar $REPOSITORY/"
@@ -30,4 +30,3 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
-    
