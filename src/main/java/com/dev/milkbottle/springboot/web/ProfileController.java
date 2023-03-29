43e2b5355f2f1a1e7eee328c7a1d6f03e759b92a
milkbottle0305 <bigtog0602@gmail.com>
2023-03-30 00:16:03 +0900

    Feat: nginx profile 수정

--- src/main/java/com/dev/milkbottle/springboot/web/ProfileController.java
+++ src/main/java/com/dev/milkbottle/springboot/web/ProfileController.java
@@ -19,7 +19,7 @@ public class ProfileController {
         List<String> realProfiles = Arrays.asList("real", "real1", "real2");
         String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
 
-        
+
         return profiles.stream()
                 .filter(realProfiles::contains)
                 .findAny()
