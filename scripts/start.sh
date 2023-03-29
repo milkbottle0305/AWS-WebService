707da02c7cc9714a9d2b5dac59bc6ba10a9dcc08
milkbottle0305 <bigtog0602@gmail.com>
2023-03-30 08:31:40 +0900

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -30,3 +30,4 @@ nohup java -jar \
     -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
     -Dspring.profiles.active=$IDLE_PROFILE \
     $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    
